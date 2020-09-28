902785e56515dbd4de36886bb554ba873918ea23
Iván <ivan.gw@hotmail.com>
2020-09-28 18:51:46 -0500

    Operaciones lógicas

--- Practicas/BitWise.c
+++ Practicas/BitWise.c
@@ -4,29 +4,28 @@
 void main ()
 {
     int CuantosBits;
-    char Cualop;
+    int Cualop;
+    int Switch=1;
     printf("\n¿De cuántos bits es tu variable?\n 8 bits \n 16 bits \n 32 bits\n");
     scanf("%d",&CuantosBits);
 
 if (CuantosBits==8||CuantosBits==16||CuantosBits==32)
 	{
-	   printf("\n¿Qué operación deseas hacer?\n A)AND\n O)OR\n X)XOR");
-	   scanf("%c",&Cualop);
-		if (Cualop=='A')
-		{
-		
-		}
-		else if (Cualop=='O')
-		{
-			
-		}
-		else if (Cualop=='X')	
-		{
-
-		}
-    }
-
-
+        do{
+	        printf("\n¿Qué operación deseas hacer?\n 1)AND\n 2)OR\n 3)XOR\n");
+	        scanf("%d",&Cualop);
+		        if (Cualop>=1 && Cualop<=3)
+	                 {
+	    
+    	             }
+    
+        else 
+            {
+            printf("\nOpción no válida, vuelva a intentarlo");
+            }
+        
+        }while (Switch==1);
+	}
 else 
 	{printf("\nLa variable debe ser de 8, 16 o 32 bits.");
 	}
