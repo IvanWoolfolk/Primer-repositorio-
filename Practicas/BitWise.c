7df30c66efe6d4ad9265c4df257bd1ce3f50fb19
Esteban <estebanponce1@hotmail.com>
2020-09-29 10:43:03 -0500

    Operaciones_Esteban

--- Practicas/BitWise.c
+++ Practicas/BitWise.c
@@ -3,29 +3,37 @@
 #include <conio.h>
 void main ()
 {
-    int Bits,corrimiento;
+    int Bits,corrimiento,ValueToChange;
     int Cualop;
     int Switch=1;
 	unsigned char u8variable8;
 	unsigned short u16variable16;
-	unsigned long u32variable;
+	unsigned long u32variable32;
     printf("\n¿De cuántos bits es tu variable?\n 8 bits \n 16 bits \n 32 bits\n");
     scanf("%d",&Bits);
 
 if (Bits==8||Bits==16||Bits==32)
 	{
-        	do{
-	        	printf("\n¿Qué operación deseas hacer?\n 1)AND\n 2)OR\n 3)XOR\n");
+        do{
+            if (Bits==8)
+			    ValueToChange=u8variable8;
+			else if(Bits==16)
+			    ValueToChange=u16variable16;
+			else
+			    ValueToChange=u32variable32;
+				
+	        	printf("\n¿Qué operación deseas hacer?\n 1)AND\n 2)OR\n 3)XOR: ");
 	        	scanf("%d",&Cualop);
 		        	if (Cualop>=1 && Cualop<=3)
 	                 	{
-					     printf("Escoja el corrimiento de la operación");
+					     printf("\nEscoja el corrimiento de la operación: ");
 						 scanf("%d",&corrimiento);
+						 printf("\nIngrese el numero a cambiar: ");
+						 scanf("%d",&ValueToChange);
 
 	                     switch(Cualop)
 						 {
 							 case 1:
-							 printf("\nIngrese el valor a cambiar");
 							 break;
 
 							 case 2:
@@ -34,7 +42,7 @@ if (Bits==8||Bits==16||Bits==32)
 							 case 3:
 							 break;
 						 }
-    	             	}
+    	    }
     
         			else 
             			{printf("\nOpción no válida, vuelva a intentarlo");}
