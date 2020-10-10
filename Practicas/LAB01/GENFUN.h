6217bd53f863ae6e955514210e604518fc1cdfa5
Iván <ivan.gw@hotmail.com>
2020-10-09 20:32:28 -0500

    Acomodar GENFUN Y PRACTICA1

--- Practicas/LAB01/GENFUN.h
+++ Practicas/LAB01/GENFUN.h
@@ -1,15 +1,20 @@
-//DATA TYPES//
+/*DATA TYPES*/
 
 typedef unsigned char uint8;
 typedef unsigned char uint16;
 typedef unsigned char uint32;
 
-//Definitions//
+/*Definitions*/
 #define ASCII_HIGH_THRESHOLD_OFF_ON 123
 #define ASCII_LOW_THRESHOLD_OFF_ON 96
-#define ASCII_HIGH_THRESHOLD_ON_OFF 91
-#define ASCII_LOW_THRESHOLD_ON_OFF 64
 #define ASCII_CONVERTION_FACTOR 32
-
-//Function Prototypes//
+#define ASCII_LOW_THRESHOLD_OFF_OFF 64
+#define ASCII_HIGH_THRESHOLD_OFF_OFF 91
+#define TARGET_VALUE 111                   //111ASCI='o'
+#define VALUE_ARRAY 11
+#define ARRAY_SIZE 12
+/*Function Prototypes*/
 void GENFUN_vCAPSON (uint8 *pu8Src, uint8 u8SizeOfList);
+void GENFUN_vCAPSOFF (uint8 *pu8Src, uint8 u8SizeOfList);
+uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList);
+void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList);
--- Practicas/LAB01/Practica1.c
+++ Practicas/LAB01/Practica1.c
@@ -1,36 +1,9 @@
-#include "stdlib.h"
 #include "stdio.h"
-//#include "GENFUN.h"
-/*DATA TYPES*/
+#include "GENFUN.h"
+#include <stdlib.h>
+#include <time.h> 
 
-typedef unsigned char uint8;
-typedef unsigned char uint16;
-typedef unsigned char uint32;
-
-/*Definitions*/
-#define ASCII_HIGH_THRESHOLD_OFF_ON 123
-#define ASCII_LOW_THRESHOLD_OFF_ON 96
-#define ASCII_CONVERTION_FACTOR 32
-#define ASCII_LOW_THRESHOLD_OFF_OFF 64
-#define ASCII_HIGH_THRESHOLD_OFF_OFF 91
-#define TARGET_VALUE 111                   //111ASCI='o'
-#define VALUE_ARRAY 11
-#define ARRAY_SIZE 12
-/*Function Prototypes*/
-void GENFUN_vCAPSON (uint8 *pu8Src, uint8 u8SizeOfList);
-void GENFUN_vCAPSOFF (uint8 *pu8Src, uint8 u8SizeOfList);
-uint8 GENFUN_u8GetOccurence (uint8 *pu8Src, uint8 u8Target, uint8 u8SizeOfList);
-void GENFUN_u8MemSet (uint8 *pu8Src, uint8 u8Char2Set, uint8 u8SizeOfList);
-
-
-
-
-
-
-
-
-
-//************************divisor*********************************************//
+//***********************Practica*********************************************//
 void main(void)
 {
     uint8 u8VALOR;
