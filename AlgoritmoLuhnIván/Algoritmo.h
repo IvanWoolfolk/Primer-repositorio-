ae933869953cf870452eb007a1bf47de5c4cde23
Iván <ivan.gw@hotmail.com>
2020-10-19 13:03:12 -0500

    Dos carpetas

---
+++ AlgoritmoLuhnIván/Algoritmo.h
@@ -0,0 +1,13 @@
+/*typedef unsigned*/
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+/*Defines*/
+#define ASCII_HIGH_THRESHOLD_OFF_ON 123 
+#define ASCII_LOW_THRESHOLD_OFF_ON 96 
+#define ASCII_CONVERTION_FACTOR 32 
+
+/*FunctionsPrototypes*/
+void GENFUNvCapsOn (unint8 *pu8Src, uint8 u8SizeOfList);
+
