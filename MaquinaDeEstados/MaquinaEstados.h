7d3e9d40dce2265df21b68f718df422b69618508
Iván <ivan.gw@hotmail.com>
2020-11-21 16:35:57 -0600

    bat

--- MaquinaDeEstados/MaquinaEstados.h
+++ MaquinaDeEstados/MaquinaEstados.h
@@ -6,25 +6,33 @@ void TABLERO_CUBIERTO();
 
 /***********************************defines************************************/
 #define APAGADO 0
-#define MOJITO 1
-#define GINTONIC 2
-#define CAIPIRINHA 3
-#define PIÑACOLADA 4
-#define ASCII_A 65
-#define ASCII_BASE0 48
-#define UNKNOWN_CAGE '#'
-#define MINE '@'
-#define MAX_RAND 3
-#define TOTAL_CAGES 255
-
+#define TRAGO1 1
+#define TRAGO2 2
+#define TRAGO3 3
+#define TRAGO4 4
+#define TRAGO5 5
+#define TRAGO6 6
+#define TRAGO7 7
+#define TRAGO8 8
+#define TRAGO9 9
+#define idle 0
+#define	enTequila 16
+#define	enTripleseco 17
+#define	enLimon 18
+#define	enRon 19
+#define	enGinebra 20
+#define	enWhisky 21
+#define	enTonica 22
+#define	enAguagas 23
+#define	enRonb 24
+#define	enVodka 25 
+#define	enCafe 26 
+#define	enVermut 27
+#define	enPina 28
+#define	enHielo 29
+#define	enCoco 30
 /*******************************typedefines************************************/
 typedef unsigned char uint8;
 typedef unsigned short uint16;
 typedef unsigned int uint32;
 
-typedef struct
-{
-    unsigned int u8TOTAL_MINAS;
-    unsigned int u8MINAS_FALTANTES;
-    uint8 u8MINAS_DESCUBIERTAS;
-}MINESinfo;
--- MaquinaDeEstados/Menu.c
+++ MaquinaDeEstados/Menu.c
@@ -4,29 +4,360 @@
 
 void main(void)
 {
-	uint8 TRAGO;
-	uint8 ENCENDIDO;
-	while(ENCENDIDO!=APAGADO){
+	uint8 TRAGO=0;
+	uint8 ENCENDIDO=1;
+	uint8 CurrentState=0;
+	uint8 enSecondState;
+	uint8 enThirdState;
+	uint8 enCurrentState = idle;
+	while(ENCENDIDO!=APAGADO)
+	{
+		CurrentState+=1;
+		if (CurrentState<=3)
+		{
+			switch ( enCurrentState )
+			{
+				case idle:
+				{	//**Menú**//
+					printf("\nBuen Dia, ¿Que le gustaria que le sirva?\n1)Margarita\n2)Mojito\n3)Gintonic\n4)BlackRussian\n5)Manhattan\n6)Piña colada\n7)Daiquiri\n8)Cosmopolitan\n9)Martini\n");
+					scanf("%d",&TRAGO);
 
-		printf("Buen Día, ¿Qué le gustaría que le sirva?");
-		scanf("%d",&TRAGO);	
+					if(TRAGO==TRAGO1)
+					{//***Margarita**//
+						printf("Selecciono Margarita");
+						enCurrentState = enTequila;
+						enSecondState = enTripleseco;
+						enThirdState = enLimon;
+					}
+					else if(TRAGO==TRAGO2)
+					{//***Mojito**//
+						printf("Selecciono Mojito");
+						enCurrentState = enRon;
+						enSecondState=enLimon;
+						enThirdState=enAguagas;
+					}	
+					else if (TRAGO==TRAGO3)
+					{//***Gintonic**//
+						printf("Selecciono Gintonic");
+						enCurrentState = enGinebra;
+						enSecondState=enTonica;
+						enThirdState=enHielo;
+					}
+					else if (TRAGO==TRAGO4)
+					{//***BlackRussian**//
+						printf("Selecciono BlackRussian");
+						enCurrentState = enVodka;
+						enSecondState=enCafe;
+						enThirdState=enHielo;
+					}
+					else if (TRAGO==TRAGO5)
+					{//***Manhattan**//
+						printf("Selecciono Manhattan");
+						enCurrentState = enWhisky;
+						enSecondState=enVermut;
+						enThirdState=enHielo;
+					}
+					else if (TRAGO==TRAGO6)
+					{//***Piña colada**//
+						printf("Selecciono Piña Colada");
+						enCurrentState = enRon;
+						enSecondState=enPina;
+						enThirdState=enCoco;
+					}
+					else if (TRAGO==TRAGO7)
+					{//***Daiquiri**//
+						printf("Selecciono Daiquiri");
+						enCurrentState = enRonb;
+						enSecondState=enLimon;
+						enThirdState=enHielo;
+					}
+					else if (TRAGO==TRAGO8)
+					{//***Cosmopolitan**//
+						printf("Selecciono Cosmopolitan");
+						enCurrentState = enVodka;
+						enSecondState=enTripleseco;
+						enThirdState=enLimon;
+					}
+					else if (TRAGO==TRAGO9)
+					{//***Martini**//
+						printf("Selecciono Martini");
+						enCurrentState = enGinebra;
+						enSecondState=enVermut;
+						enThirdState=enHielo;
+					}
+					else
+					{
+						printf("Opcion no válida");
+						enCurrentState = idle;
+					}
+					break;
+				}
 
-		if(TRAGO==MOJITO) {
-			
+				case enTequila: 
+				{
+					printf("\nSe sirve Tequila");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enTripleseco: 
+				{
+					printf("\nSe sirve Triple seco");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}	
+				case enRon: 
+				{
+					printf("\nSe sirve Ron");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enLimon:
+				{
+					printf("\nSe sirve zumo de limón");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enAguagas:
+				{
+					printf("\nSe sirve Agua Mineral");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enTonica:
+				{
+					printf("\nSe sirve Tonica");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enHielo:
+				{
+					printf("\nSe sirve Hielo");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enVodka:
+				{
+					printf("\nSe sirve Vodka");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enCafe:
+				{
+					printf("\nSe sirve licor de café");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enVermut:
+				{
+					printf("\nSe sirve Vermut");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enWhisky:
+				{
+					printf("\nSe sirve Whisky");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enPina:
+				{
+					printf("\nSe sirve Piña");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enCoco:
+				{
+					printf("\nSe sirve Crema de Coco");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enRonb:
+				{
+					printf("\nSe sirve Ron Blanco");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
+				case enGinebra:
+				{
+					printf("\nSe sirve Ginebra");
+					if (CurrentState==1)
+					{
+						enCurrentState==enSecondState;
+					}
+					else if (CurrentState==2)
+					{
+						enCurrentState==enThirdState;
+					}
+					else 
+					{
+						//**Nothing to do**//
+					}
+					break;
+				}
 
+				default:
+				break;
+			}
 		}
-		else if(TRAGO==GINTONIC) {
-			
 
-		}	
-		else if(TRAGO==CAIPIRINHA) {
-		
-
+		else
+		{
+			enCurrentState = idle;
 		}
-		else if(TRAGO==PIÑACOLADA) {
-		
 
-		}
 	}
-	
 }
