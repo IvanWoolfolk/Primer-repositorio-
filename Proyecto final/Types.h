633445193a43e1fa5cad374c51cc18b10560eeca
Iván <ivan.gw@hotmail.com>
2020-12-05 10:47:51 -0600

    Segundo commit elevador

--- Proyecto final/Types.h
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-
-
-/*******************************typedefines************************************/
-typedef unsigned char uint8;
-typedef unsigned short uint16;
-typedef unsigned int uint32;
-
